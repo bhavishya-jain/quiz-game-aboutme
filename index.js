87cc486394a5e08c2ead143fc6cf52139ca3b72d
BhavishyaJain1 <91150665+bhavishya-jain@users.noreply.github.com>
2021-09-28 18:07:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+
+var readlineSync = require("readline-sync");
+
+var score = 0;
+
+// array of objects
+var questions = [{
+  question: "Where do I live? ",
+  answer: "Indore"
+},
+{
+  question: "When is my birthday? ",
+  answer: "31 January"
+},
+{
+  question: "Which cuisine do I like the most? ",
+  answer: "Indian"
+},
+{
+  question: "what is my favourite Indian holiday destination? ",
+  answer: "Goa"
+},
+{
+  question: "Which domain do I like the most? ",
+  answer: "Web Development"
+}];
+
+function welcome() {
+ var userName = readlineSync.question("What's your name? ");
+
+  console.log("Welcome "+ userName + " to HOW WELL DO YOU KNOW Bhavishya?- Quiz Game");
+}
+
+
+// play function
+function play(question, answer) {
+  var userAnswer = readlineSync.question(question);
+
+  if (userAnswer.toUpperCase() === answer.toUpperCase()) { // branching
+    console.log("You are right!");
+    score = score + 1;
+    
+  } else {
+    console.log("You are wrong!");
+   
+  }
+
+  console.log("current score: ", score);
+  console.log("-------------")
+}
+
+function game() {
+  for (var i=0; i<questions.length; i++) {
+    var currentQuestion = questions[i];
+    play(currentQuestion.question, currentQuestion.answer)
+  }
+}
+
+function dispScore() {
+  console.log("Your Final Score: ", score);
+  console.log("Thanks for playing!")
+}
+
+
+welcome();
+game();
+dispScore();
